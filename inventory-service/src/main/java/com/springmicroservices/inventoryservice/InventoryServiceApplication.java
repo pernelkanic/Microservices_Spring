f728c526cacc6c030b8ea9d6b5697fdd32d2eae7
pernelkanic <whitehattt03@gmail.com>
2023-11-21 12:42:59 +0530

    added docker-compose

--- inventory-service/src/main/java/com/springmicroservices/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/springmicroservices/inventoryservice/InventoryServiceApplication.java
@@ -1,5 +1,6 @@
 	package com.springmicroservices.inventoryservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,25 +12,11 @@ import com.springmicroservices.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class InventoryServiceApplication {
+public class InventoryServiceApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-        return args -> {
-            Inventory inventory = new Inventory();
-            inventory.setSkuCode("iphone_13");
-            inventory.setQuantity(100);
-
-            Inventory inventory1 = new Inventory();
-            inventory1.setSkuCode("iphone_13_red");
-            inventory1.setQuantity(0);
-
-            inventoryRepository.save(inventory);
-            inventoryRepository.save(inventory1);
-        };
+  
     }
-}
+
---
+++ inventory-service/src/main/java/com/springmicroservices/inventoryservice/Utils/DataLoader.java
@@ -0,0 +1,31 @@
+package com.springmicroservices.inventoryservice.Utils;
+
+
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.springmicroservices.inventoryservice.model.Inventory;
+import com.springmicroservices.inventoryservice.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+    private final InventoryRepository inventoryRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        Inventory inventory = new Inventory();
+        inventory.setSkuCode("iphone_13");
+        inventory.setQuantity(100);
+
+        Inventory inventory1 = new Inventory();
+        inventory1.setSkuCode("iphone_13_red");
+        inventory1.setQuantity(0);
+
+        inventoryRepository.save(inventory);
+        inventoryRepository.save(inventory1);
+    }
+}
